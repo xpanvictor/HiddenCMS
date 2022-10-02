999f79ed4d431b1ad0171e46e687bc0724d92cbc
xpanvictor <xpanvictor@gmail.com>
2022-10-02 03:46:38 +0100

    Init: basic server

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
-let user: string = 'Victor'
-console.log(user)
+require('dotenv').config()
+
+const http = require('http')
+
+
+const server = http.createServer()
+
+async function startServer () {
+    server.listen(process.env.PORT, () => console.log('Hidden cms v1.0 running at port: ', process.env.PORT))
+}
+
+startServer()
+    .then(()=>console.log('started'))
+    .catch(e => console.log(e))
