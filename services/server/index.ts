01426c4c98dfaf946968c6a1634e66d79fb63334
xpanvictor <xpanvictor@gmail.com>
2022-10-13 18:37:40 +0100

    Feat: Connecting mongo db

--- services/server/index.ts
+++ services/server/index.ts
@@ -4,11 +4,14 @@ const http = require('http')
 const app = require('./Main')
 const {handleUnhandledRejection, handleUncaughtExceptions} = require('./error/serverError')
 
+import ConnectDB from './db/connect'
+
 const server = http.createServer(app)
 
 async function startServer () {
     handleUnhandledRejection(server)
     handleUncaughtExceptions()
+    await ConnectDB()
     server.listen(process.env.PORT, () => console.log('😎Hidden cms v1.0 running at port:', process.env.PORT))
 }
 
