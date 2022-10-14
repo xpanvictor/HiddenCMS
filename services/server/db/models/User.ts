988a45215e6914998908451f8559a62a88d5c385
xpanvictor <xpanvictor@gmail.com>
2022-10-14 21:43:37 +0100

    Feat: user model created

---
+++ services/server/db/models/User.ts
@@ -0,0 +1,67 @@
+import mongoose, {Model, Schema} from 'mongoose'
+const bcrypt = require('bcrypt')
+
+interface IUser {
+    name: string,
+    email: string,
+    password: string,
+    permission: 'user' | 'author' | 'admin',
+    favourites: Array<string | undefined>
+}
+
+interface IUserMethods {
+    comparePassword(password: string): boolean
+}
+
+type UserModel = Model<IUser, {}, IUserMethods>
+
+const UserSchema = new Schema<IUser, UserModel, IUserMethods>({
+    name: {
+        type: String,
+        default: 'Anonymous Hiddener',
+        index: true,
+        validate: {
+            validator: (v: string) => /\w*\s\w*/i.test(v),
+            message: 'Please enter full first name!'
+        }
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        validate: {
+            validator: (e: string) => /^\S+@\S+\.\S+$/i.test(e),
+            message: 'Enter valid email!'
+        }
+    },
+    password: {
+        type: String,
+        minlength: [8, 'Password must be at least 8 chars long!'],
+        required: [true, 'Password is required!'],
+    },
+    permission: {
+        type: String,
+        enum: ['user', 'author', 'admin'],
+        default: 'user'
+    },
+    favourites: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Post'
+    }
+})
+
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next()
+    try{
+        const salt = bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+        return next()
+    }catch (err: any) {
+        return next(err)
+    }
+})
+
+UserSchema.methods.comparePassword = function(password) {
+    return bcrypt.compare(password, this.password)
+}
+
+module.exports = mongoose.model<IUser, UserModel>('User', UserSchema)
